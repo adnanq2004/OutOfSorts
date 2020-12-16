6f6de70d576f3667ad31357b8df2b20772903efc
adnanq2004 <aquayyum20@stuy.edu>
2020-12-16 16:25:30 -0500

    finally got code working. made it change values instead of just changing variables that held the values of the places

--- Sorts.java
+++ Sorts.java
@@ -2,26 +2,29 @@ public class Sorts{
 
   public static void bubbleSort(int[] data) {
     int n = data.length;
-    while (n > 2) {
-      int swaps = 0;
-      for (int i = 0; i < n-1; i++) {
-        int place1 = data[i];
-        int place2 = data[i+1];
-        if (place1 > place2) {
-          int temp = place2;
-          place2 = place1;
-          place1 = temp;
-          swaps++;
-        }
-      }
-      if (swaps == 0) {
-        break;
-      }
-      else {
-        n--;
-      }
-    }
+    int place1 = 0;
+    int place2 = 0;
+    int temp = 0;
+    int swaps = 0;
+    while (n >= 2) {
+      	swaps = 0;
+	for (int i = 0; i < n-1; i++) {
+        	place1 = data[i];
+        	place2 = data[i+1];
+        	if (place1 > place2) {
+          		temp = place2;
+          		data[i+1] = place1;
+          		data[i] = temp;
+          		swaps++;
+		}
+	}
+      	if (swaps == 0) {
+		n = 0;
+      	}
+      	n--;
   }
+
+ }
 
 
 }
